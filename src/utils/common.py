37599568065b6f04205712dff424bb71fe34b20a
Bibek Rauth <rauth.bibek007@gmail.com>
2021-10-30 21:30:07 +0530

    stage 01 base implementation

---
+++ src/utils/common.py
@@ -0,0 +1,8 @@
+import logging
+import yaml 
+
+def read_yaml(path_to_yaml:str) -> dict:
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+    logging.info(f"yaml  file: {path_to_yaml} loaded successfully")
+    return content
